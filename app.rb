9e0090f591c6d9d1075754b5c08c4010c8a4f6be
Corrie Smith & Josue Valverde <student@epicodus.com>
2015-08-17 16:44:02 -0700

    add html forms and displays.

--- app.rb
+++ app.rb
@@ -19,3 +19,27 @@ end
 get('/restaurants') do
   erb(:restaurants)
 end
+
+post('/restaurants') do
+  name = params.fetch('name')
+  location = params.fetch('location')
+  phone = params.fetch('phone')
+  id = params.fetch('id')
+  @restaurant = Restaurant.new({:name => name, :location => location, :phone => phone, :id => id})
+  @restaurant.save()
+  erb(:restaurants)
+end
+
+get('/restaurants/:id') do
+  @restaurant_id = params.fetch('id')
+  erb(:foods)
+end
+
+post('/restaurants/:id') do
+  type = params.fetch('type')
+  cost = params.fetch('cost')
+  rating = params.fetch('rating')
+  @food = Food.new({:type => type, :cost => cost, :rating => rating})
+  @food.save()
+  erb(:foods)
+end
